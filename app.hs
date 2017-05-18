79bb128f2fb41ed8cbeb4035e4dd00a63a747185
necia425 <necia425@gmail.com>
2017-05-18 23:59:05 +0200

    metoda niedzialajaca

--- app.hs
+++ app.hs
@@ -1,13 +1,15 @@
 module App where
 import System.Environment 
 import Plaster
+import Data.Maybe
 
 main = do
-    contents <- readFile "example.txt"
+    contents <- readFile "example1.txt"
     if checkInput contents then do
         let rowLines = lines contents
         let plaster = parsePlaster rowLines
-        return plaster
+        let solvedPlaster = solveWithPrepare plaster
+        return (plaster, solvedPlaster)
     else error "Niepoprawne wejście"
     
     
