48b92bf207732dab0341ee31a0c62fd7408e3181
necia425 <necia425@gmail.com>
2017-05-28 17:13:19 +0200

    Przygotowanie pliku odpalajacego

--- app.hs
+++ app.hs
@@ -3,21 +3,20 @@ import System.Environment
 import Plaster
 
 main = do
-    contents <- readFile "example1.txt"
+    contents <- readFile "example.txt"
     if checkInput contents then do
         let rowLines = lines contents
         let plaster @ (Plaster rows size) = parsePlaster rowLines
+        print "Plaster wejsciowy"
         print plaster
-        print size
-        let array = plasterToArray plaster
-        print array
-        print (isPlasterCorrect plaster)
-        let solved = solve [array] size
-        print solved
-        if (length solved) > 0 then
-        	print (fromArrayToPlaster (solved !! 0) size)
-        else
-        	print "Brak rozwiazan"
+        if not (isPlasterCorrect plaster) then
+            print "Plaster wejsciowy niepoprawny"
+        else do
+            let array = plasterToArray plaster
+            let solved = solve [array] size
+            let firstSolved = solved !! 0
+            print "Plaster rozwiazany"
+            print (fromArrayToPlaster firstSolved size)
         return plaster
     else error "Niepoprawne wejście"
     
